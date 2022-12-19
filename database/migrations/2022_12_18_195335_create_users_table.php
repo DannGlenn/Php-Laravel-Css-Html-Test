65be9d0db32066763b2fcc9e3391be1fb71974f7
danielglencer <danielglencer@gmail.com>
2022-12-19 09:25:18 +0200

    unique username

--- database/migrations/2022_12_18_195335_create_users_table.php
+++ database/migrations/2022_12_18_195335_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
+            $table->string('username')->unique();
             $table->string('password');
         });
     }
